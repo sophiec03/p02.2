842e3dfc18c0a5980983628174e2b21b1f9251ed
Andrew Charlton <a.charlton32@gmail.com>
2016-10-12 07:02:11 +0100

    Added substring

---
+++ substring.py
@@ -0,0 +1,42 @@
+"""
+Problem:
+
+    Given a string, s, we can compose a substring using
+    every second letter, starting with the first character.
+    eg. "HelloWorld" -> "Hlool"
+
+    The function 'sub_search' takes in two strings, s and
+    word.
+
+    If word is present in s - print "Found in original"
+    If word is present in the substring - print "Found in substring"
+    Otherwise, print "Not found"
+
+    The search should be case-insensitive.
+
+Tests:
+
+    >>> sub_search("No", "Dont stop me now!")
+    Found in original
+    >>> sub_search("hag", "I'm having such a good time")
+    Found in substring
+    >>> sub_search("vin", "Im having a ball")
+    Found in original
+    >>> sub_search("ago", "If you want to have a good time just give me a call")
+    Found in substring
+    >>> sub_string("fun", "Dont stop me now!")
+    Not found
+
+"""
+
+# Use this to test your solution. Don't edit it!
+import doctest
+def run_tests():
+    doctest.testmod(verbose=True)
+
+
+# Edit this code
+def sub_search(word, s):
+
+
+    
