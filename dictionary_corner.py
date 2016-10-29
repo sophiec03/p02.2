3e3807af2c18d480087455b7c9aecfdd7b039fd8
sophiec03 <sophie.sc2@gmail.com>
2016-10-29 11:40:06 +0100

    dictionary corner
    4 tests passed

--- dictionary_corner.py
+++ dictionary_corner.py
@@ -39,4 +39,22 @@ def run_tests():
 
 # Edit this code
 def dictionary(word1, word2):
+    
+     if word1 == word2:
+        print("You're there!")
 
+    elif word1.lower() == word2.lower():
+        print("You're there!")
+
+    elif word1.upper() == word2.upper():
+        print("You're there!")
+
+    elif word1 <= word2:
+        print("Go back")
+
+    elif word1 >= word2:
+        print("Go back")
+
+    
+    else:
+        print("Keep going")
